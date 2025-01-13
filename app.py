55c200a29a8e7f8a5e4b2185cab73440521470fc
Sergio <recursoswebamadeo@gmail.com>
2025-01-13 12:33:17 +0100

    v1.0

--- app.py
+++ app.py
@@ -1,124 +1,139 @@
 from flask import Flask, render_template, redirect, request, session
 from flask_session import Session
-from cs50 import SQL
 import os
 import gdown
+import requests
+import json
 
-ruta_archivo_local = 'WDI.db'
-
-if not os.path.exists(ruta_archivo_local):
-    url_archivo_drive = 'https://drive.google.com/uc?id=1zM2DSi0Wh5cNYmkxl4iffe1W_bBOiBlE'
-    gdown.download(url_archivo_drive, ruta_archivo_local, quiet=False)
-else:
-    print("El archivo ya existe localmente. No se realizará ninguna descarga.")
-
+# URL de la base de datos de Turso y token de autenticación
+DB_URL = "https://globaldata-sergiosoftdev2.turso.io/v2/pipeline"
+DB_TOKEN = "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MzY3NjIwMTcsImlkIjoiMjhjMDAwYWItNDg4Mi00N2JlLTlmY2ItOWMxOGUwNzcyOTVmIn0.fo2XYrBUaKckAOxrBck03ohb9iocQkTcBUTqiBBnMkz1AAYnbVMKbfYbPZESXTQhtrqr3wQsh3rErI4i2zJ7AQ"
 
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///WDI.db")
-
+# Helper function to make requests to Turso API
+def turso_request(query):
+    headers = {
+        'Authorization': f'Bearer {DB_TOKEN}',
+        'Content-Type': 'application/json'
+    }
+    data = {
+        "requests": [
+            {"type": "execute", "stmt": {"sql": query}},
+            {"type": "close"}
+        ]
+    }
+    response = requests.post(DB_URL, headers=headers, json=data)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
+        return None
 
 selectedcountry = ""
 
-
 @app.route('/', methods=["GET", "POST"])
 def hello_world():
-
-   if  request.method == "GET":
-
+    if request.method == "GET":
         print("success")
-
-
         return render_template("/index.html")
 
-   elif request.method == "POST":
-
-       inputform = request.form.get("inputform")
-
-       print(inputform)
-
-       return render_template("/index.html")
+    elif request.method == "POST":
+        inputform = request.form.get("inputform")
+        print(inputform)
+        return render_template("/index.html")
 
 @app.route("/trycountries", methods=["GET", "POST"])
 def trycountries():
-
     if request.method == "GET":
-
         countries = []
+        # Hacer consulta a la API de Turso en lugar de usar SQLite
+        query = "SELECT ShortName FROM WDICountry"
+        getcountries = turso_request(query)
+        
 
-        getcountries = db.execute("SELECT ShortName FROM WDICountry")
+        newCountries = getcountries["results"][0]['response']['result']['rows']
 
-        for i in getcountries:
-            i = i["ShortName"]
-            countries.append(i)
+        if getcountries:
+            for i in newCountries:
+                countries.append(i[0]["value"])
 
         return render_template("/trycountries.html", countries=countries)
 
     if request.method == "POST":
-
         selectedcountry = request.form.get("selectedcountry")
         session["country"] = selectedcountry
 
-        getcountrycode = db.execute("SELECT CountryCode FROM WDICountry WHERE ShortName = ?", session["country"])[0]["CountryCode"]
-        session["countrycode"] = getcountrycode
+        # Obtener el CountryCode para el país seleccionado
+        query = f"SELECT CountryCode FROM WDICountry WHERE ShortName = '{session['country']}'"
+        getcountrycode = turso_request(query)
+
+        print(getcountrycode)
+        
+        if getcountrycode:
+            session["countrycode"] = getcountrycode['results'][0]['response']['result']["rows"][0][0]["value"]
 
         print(session["country"])
         print(session["countrycode"])
 
         return redirect("/tryseries")
 
-@app.route("/tryseries", methods=["POST","GET"])
+@app.route("/tryseries", methods=["POST", "GET"])
 def tryseries():
-
     if request.method == "GET":
-
         series = []
+        # Obtener las series para el país seleccionado
+        query = f"SELECT IndicatorName FROM WDIData WHERE CountryCode = '{session['countrycode']}'"
+        getseries = turso_request(query)
 
-        getseries = db.execute("SELECT IndicatorName FROM WDIData WHERE CountryCode = ?", session["countrycode"])
+        print(getseries)    
 
-        for i in getseries:
-            series.append(i["IndicatorName"])
+        getNewSeries = getseries['results'][0]['response']['result']["rows"]
 
-        serieskeys = list(getseries[0].keys())
+        if getseries:
+            for i in getNewSeries:
+                series.append(i[0]["value"])
 
-        return render_template("/tryseries.html", series=series, serieskeys=serieskeys)
+        return render_template("/tryseries.html", series=series)
 
     if request.method == "POST":
-
         gettheseries = request.form.get("selectedseries")
-        getdata = db.execute("SELECT * FROM WDIData WHERE CountryCode = ? AND IndicatorName = ?", session["countrycode"], gettheseries)
+        # Obtener los datos de la serie seleccionada
+        query = f"SELECT * FROM WDIData WHERE CountryCode = '{session['countrycode']}' AND IndicatorName = '{gettheseries}'"
+        getdata = turso_request(query)
 
-        session["data"] = getdata
+        if getdata:
+            session["data"] = getdata['results'][0]['response']['result']
 
         return redirect("/results")
 
-
 @app.route("/results", methods=["GET", "POST"])
 def results():
-
     if request.method == "GET":
 
         data = session["data"]
-        data = data[0]
 
-        newdata = {}
+        dataDict = {}
 
-        for key, value in data.items():
-            if value is not None:
-                newdata[key] = value
+        columns = []
+        rows = []
 
-        datakeys = list(newdata.keys())[4:]
-        datakeys = [int(i) for i in datakeys]
+        cols = data['cols']
+        rs = data['rows'][0]
 
-        datavalues = list(newdata.values())[4:]
+        for col, row in zip(cols, rs):
+            if(row["type"] == "text"):
+                dataDict[col["name"]] = row["value"]
 
+        print()
 
-        return render_template("/results.html", data=newdata, datakeys=datakeys, datavalues=datavalues)
+        datakeys = list(dataDict.keys())[4:]
+        datavalues = list(dataDict.values())[4:]
+
+        return render_template("/results.html", data=data, datakeys=datakeys, datavalues=datavalues)
 
 if __name__ == '__main__':
-
-   app.run(debug=True)
+    app.run(debug=True)
